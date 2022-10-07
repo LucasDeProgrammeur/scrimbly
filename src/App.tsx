a3543d2ae5d643bb0b07e13fe15e3d529c5458dd
Lucas Hopman <lhopman@vredo.nl>
2022-10-07 14:50:26 +0200

    Prevent text not being wrapped in tag

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,10 @@ function App() {
 
   const formatDocument = (event: React.KeyboardEvent<HTMLDivElement>) => {
     const currentKey = event.key;
-
-    console.log("keycode:" + currentKey);
-    console.log("previous key:" + previousKey);
     if (currentKey === "-" && previousKey === "-") {
       let editable = document.getElementsByClassName("editable")[0];
       event.preventDefault();
-      console.log("true");
       setContent(content.substring(0, content.length - 3) + "h");
-      console.log("InnerHTML: " + document.getElementsByClassName("editable")[0].innerHTML)
-      console.log("Length: " + document.getElementsByClassName("editable")[0].innerHTML.length)
       editable.innerHTML = editable.innerHTML.substring(0, editable.innerHTML.length - 2)
       let newElement = document.createElement("h1");
       document.getElementsByClassName("editable")[0].appendChild(newElement);
@@ -29,7 +23,6 @@ function App() {
     setPreviousKey(currentKey);
   };
 
-  console.log(content);
   return (
     <>
       <div className="draggable">
@@ -40,7 +33,20 @@ function App() {
           contentEditable="true"
           suppressContentEditableWarning={true}
           onKeyDown={(e) => {
+
             const target = e.target as HTMLInputElement;
+            if (target.innerHTML == "<br>" ||  target.innerHTML == "" ) {
+              e.preventDefault();
+              if (e.key.length == 1) {
+                target.innerHTML = "<div>" + e.key +  "</div>"
+                setEndOfContenteditable(e.target)
+              } else {
+                target.innerHTML = "<div><br></div>" 
+              } 
+
+            }
+
+            console.log(target.innerHTML)
             target.focus();
             let val = target?.innerHTML;
             formatDocument(e);
@@ -49,7 +55,7 @@ function App() {
             //e.target.setSelectionRange(e.target.innerText.length, e.target.innerText.length)
           }}
           className="editable"
-        ></div>
+        ><div><br></br></div></div>
       </div>
     </>
   );
