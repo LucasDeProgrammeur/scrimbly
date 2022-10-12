3e22094c63f021b01873a8784ecfe85ed577bac7
Lucas Hopman <lhopman@vredo.nl>
2022-10-12 16:12:21 +0200

    Tying to fix caret

--- src/helpers/insertHTMLNode.ts
+++ src/helpers/insertHTMLNode.ts
@@ -1,14 +1,45 @@
 import goToPositionContentEditable from "./goToPositionContentEditable";
 
+export default function insertHTMLNode(
+  editable: Element,
+  content: string,
+  nodeType: string,
+  event: React.KeyboardEvent<HTMLDivElement>,
+  setContent: any
+) {
 
-export default function insertHTMLNode (editable: Element, content: string, nodeType: string, event: React.KeyboardEvent<HTMLDivElement>, setContent: any) {
-event.preventDefault();
-setContent(content.substring(0, content.length - 3) + "h");
-editable.innerHTML = editable.innerHTML.substring(0, editable.innerHTML.length - 2)
-let newElement = document.createElement(nodeType);
-document.getElementsByClassName("editable")[0].appendChild(newElement);
-editable.lastChild?.previousSibling?.remove();
-newElement.textContent = "- "
-goToPositionContentEditable(editable)
+  setContent(content.substring(0, content.length - 3) + "h");
+  editable.innerHTML = editable.innerHTML.substring(
+    0,
+    editable.innerHTML.length - 2
+  );
+  let newElement = document.createElement(nodeType);
+  var range = window.getSelection();
+
+  let selectionNode = window.getSelection()?.getRangeAt(0).commonAncestorContainer;  
+
+//   let child = document
+//     .getElementsByClassName("editable")[0]
+//     .appendChild(newElement);
+
+    console.log("range: ");
+    console.log(range);   
+    range?.getRangeAt(0).insertNode(newElement);
+
+  //child.previousSibling!.remove();
+
+  
+
+//   let children = document.querySelectorAll("div");
+
+//   children.forEach(e => {
+//     console.log(e.innerHTML)
+//     if (e.innerHTML.includes("-!")) {console.log("removing");e.remove()}
+//   })
+
+  
+  
+
+  newElement.textContent = "- ";
+  goToPositionContentEditable(editable);
 }
-
