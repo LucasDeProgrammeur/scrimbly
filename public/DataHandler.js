8ab9a74557a01238ad96683362cf44fb5a5d0212
Lucas Hopman <lhopman@vredo.nl>
2023-06-01 14:41:51 +0200

    (critical udate) fix note name duplication by import

--- public/DataHandler.js
+++ public/DataHandler.js
@@ -1,95 +1,122 @@
-const sqlite3 = require('sqlite3');
-
+const sqlite3 = require('sqlite3')
 
 class DataHandler {
-  
-    constructor () {
-        this.db = new sqlite3.Database("./scrimblydb.db");
-    }
+  constructor() {
+    this.db = new sqlite3.Database('./scrimblydb.db')
+  }
 
-    getAll () {
-        this.db.run("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)", (callback) => {
-          });
-        
-          return new Promise((resolve, reject) => {
-            this.db.all("SELECT noteName, noteHTML FROM notes", (err, data) => {
-              if (err) {
-                reject(err);
-              }
-             resolve(data);
-            });
-          })
-    }
+  getAll() {
+    this.db.run(
+      'CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)',
+      (callback) => {},
+    )
 
-    updateName(newNoteName, oldNoteName) {
-      this.db.run(`UPDATE notes SET noteName = '${newNoteName}' WHERE noteName = '${oldNoteName}'`);
-    }
-
-    getAllNoteNames() {
-      this.db.run("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)", (callback) => {
-      });
-
-      return new Promise((resolve, reject) => {
-        this.db.all("SELECT id, noteName FROM notes", (err, data) => {
-          if (err) {
-            reject(err);
-          }
-         resolve(data.map(e => e["noteName"]));
-         
-        });
+    return new Promise((resolve, reject) => {
+      this.db.all('SELECT noteName, noteHTML FROM notes', (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
       })
-    }
+    })
+  }
 
-    clearDb() {
-      this.db.run("DELETE FROM notes");
-    }
+  updateName(newNoteName, oldNoteName) {
+    this.db.run(
+      `UPDATE notes SET noteName = '${newNoteName}' WHERE noteName = '${oldNoteName}'`,
+    )
+  }
 
-    getOneByName(noteName) {
-      this.db.run("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)", (callback) => {
-      });
-    
-      return new Promise((resolve, reject) => {
-        this.db.get(`SELECT noteName, noteHTML FROM notes WHERE noteName = '${noteName}'`, (err, data) => {
+  getAllNoteNames() {
+    this.db.run(
+      'CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)',
+      (callback) => {},
+    )
+
+    return new Promise((resolve, reject) => {
+      this.db.all('SELECT id, noteName FROM notes', (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data.map((e) => e['noteName']))
+      })
+    })
+  }
+
+  clearDb() {
+    this.db.run('DELETE FROM notes')
+  }
+
+  getOneByName(noteName) {
+    this.db.run(
+      'CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)',
+      (callback) => {},
+    )
+
+    return new Promise((resolve, reject) => {
+      this.db.get(
+        `SELECT noteName, noteHTML FROM notes WHERE noteName = '${noteName}'`,
+        (err, data) => {
           if (err) {
-            
-            reject(err);
+            reject(err)
           }
 
-         resolve(data);
-        });
-      })
-      
-    }
+          resolve(data)
+        },
+      )
+    })
+  }
 
-    saveOne(noteName, noteHTML) {
-      return new Promise((resolve, reject) => {
-        this.db.exec(`UPDATE notes SET noteHTML = '${noteHTML}' WHERE noteName = '${noteName}'`, (err) => {
+  async noteExists(noteName) {
+    try {
+      let result = await this.getOneByName(noteName)
+      if (result) {
+        return true
+      }
+    } catch {
+      return false
+    }
+  }
+
+  saveOne(noteName, noteHTML) {
+    return new Promise((resolve, reject) => {
+      this.db.exec(
+        `UPDATE notes SET noteHTML = '${noteHTML}' WHERE noteName = '${noteName}'`,
+        (err) => {
           if (err) {
-            reject(err);
+            reject(err)
           }
-        })
-        resolve();
-    
-      })
+        },
+      )
+      resolve()
+    })
+  }
+
+  deleteOneByName(noteName) {
+    this.db.run(`DELETE FROM notes WHERE noteName = '${noteName}'`)
+  }
+
+  async insert(noteName, noteHTML) {
+    this.db.run(
+      'CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)',
+      (callback) => {},
+    )
+    let queryString = `INSERT INTO notes (noteName, noteHTML) VALUES ('${noteName}', '${noteHTML}')`
+    let queryStringIfExists = `UPDATE notes SET noteHTML = '${noteHTML}' WHERE noteName = '${noteName}'`
+
+    let exists = await this.noteExists(noteName)
+
+    if (exists) {
+      this.db.exec(queryStringIfExists)
+      return
     }
 
-    deleteOneByName(noteName) {
-      this.db.run(`DELETE FROM notes WHERE noteName = '${noteName}'`);
-    }
+    this.db.exec(queryString)
+  }
 
-    insert(noteName, noteHTML) {
-      this.db.run("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, noteName TEXT, noteHTML TEXT)", (callback) => {
-
-      });
-      let queryString = `INSERT INTO notes (noteName, noteHTML) VALUES ('${noteName}', '${noteHTML}')`;
-      
-      this.db.exec(queryString);
-    }
-    
-    closeDb () {
-      this.db.close();
-    }
-    
+  closeDb() {
+    this.db.close()
+  }
 }
 
-module.exports = DataHandler;
+module.exports = DataHandler
--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const sqlite3 = require('sqlite3');
+const path = require('path')
+const sqlite3 = require('sqlite3')
 
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
-const isDev = require("electron-is-dev");
-let db = new sqlite3.Database("./scrimblydb.db");
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const isDev = require('electron-is-dev')
+let db = new sqlite3.Database('./scrimblydb.db')
 
-const DataHandler = require("./DataHandler");
+const DataHandler = require('./DataHandler')
 
-const handler = new DataHandler();
+const handler = new DataHandler()
 
 function createWindow() {
   // Create the browser window.
@@ -18,116 +18,114 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      preload: path.join(__dirname, 'preload.ts')
-    }
-  });
+      preload: path.join(__dirname, 'preload.ts'),
+    },
+  })
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
   win.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  )
 
   // Open the DevTools.
   if (isDev) {
-    win.webContents.openDevTools({ mode: "detach" });
+    win.webContents.openDevTools({ mode: 'detach' })
   }
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    handler.closeDb();
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    handler.closeDb()
+    app.quit()
   }
-});
+})
 
 ipcMain.handle('minimize', () => {
-  BrowserWindow.getFocusedWindow().minimize();
+  BrowserWindow.getFocusedWindow().minimize()
 })
 
 ipcMain.handle('maximize', () => {
-  BrowserWindow.getFocusedWindow().maximize();
+  BrowserWindow.getFocusedWindow().maximize()
 })
 
 ipcMain.handle('close', () => {
-  BrowserWindow.getFocusedWindow().close();
+  BrowserWindow.getFocusedWindow().close()
 })
 
 ipcMain.handle('restore', () => {
-  BrowserWindow.getFocusedWindow().restore();
+  BrowserWindow.getFocusedWindow().restore()
 })
 
 ipcMain.handle('isMaximized', () => {
-  return BrowserWindow.getFocusedWindow().isMaximized();
+  return BrowserWindow.getFocusedWindow().isMaximized()
 })
 
 ipcMain.handle('updateName', (e, args) => {
-  let newNoteName = args[0];
-  let oldNoteName = args[1];
+  let newNoteName = args[0]
+  let oldNoteName = args[1]
   handler.updateName(newNoteName, oldNoteName)
 })
 
-ipcMain.handle('getAll', () => { return handler.getAll()});
+ipcMain.handle('getAll', () => {
+  return handler.getAll()
+})
 
 ipcMain.handle('getAllNoteNames', () => {
-  return handler.getAllNoteNames();
-});
-
-ipcMain.handle("clearDb", () => {
-  return handler.clearDb();
+  return handler.getAllNoteNames()
 })
 
+ipcMain.handle('clearDb', () => {
+  return handler.clearDb()
+})
 
 ipcMain.handle('getOneByName', (e, args) => {
-  let noteName = args[0];
-  return handler.getOneByName(noteName);
-});
-
-ipcMain.handle("saveOne", (e, args) => {
-  let noteName = args[0];
-  let noteHTML = args[1];
-  return handler.saveOne(noteName, noteHTML);
+  let noteName = args[0]
+  return handler.getOneByName(noteName)
 })
 
-ipcMain.handle("deleteOneByName", (e, args) => {
-  let noteName = args[0];
-  db.run(`DELETE FROM notes WHERE noteName = '${noteName}'`);
+ipcMain.handle('saveOne', (e, args) => {
+  let noteName = args[0]
+  let noteHTML = args[1]
+  return handler.saveOne(noteName, noteHTML)
 })
 
-ipcMain.handle("insert", (e, args) => {
-  let noteName = args[0];
-  let noteHTML = args[1];
-  return handler.insert(noteName, noteHTML);
-});
+ipcMain.handle('deleteOneByName', (e, args) => {
+  let noteName = args[0]
+  db.run(`DELETE FROM notes WHERE noteName = '${noteName}'`)
+})
 
-
+ipcMain.handle('insert', async (e, args) => {
+  let noteName = args[0]
+  let noteHTML = args[1]
+  return await handler.insert(noteName, noteHTML)
+})
 
 ipcMain.handle('fileOpenExport', () => {
-  return dialog.showOpenDialog({properties: ['openDirectory']});
+  return dialog.showOpenDialog({ properties: ['openDirectory'] })
 })
 
 ipcMain.handle('fileOpenImport', () => {
-  return dialog.showOpenDialog({properties: ['openFile']});
+  return dialog.showOpenDialog({ properties: ['openFile'] })
 })
 
-
-app.on("activate", () => {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
